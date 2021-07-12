5669336192a00cf0415a93257fe3add0c3587947
Toppedyk <Tayloroppedyk@yahoo.com>
2021-07-12 09:14:19 -0600

    line break in string

--- App.py
+++ App.py
@@ -1,9 +1,11 @@
-# Strings use quotes when declared
-name="Taylor"
-# integers don't use quotes when being declared
-age = 26
+# # Strings use quotes when declared
+# name="Taylor"
+# # integers don't use quotes when being declared
+# age = 26
 
-print(name+' is a boy')
-# int must use comma, string can use either
-print(name+' is', age)
-print(name+' is from Idaho')
+# print(name+' is a boy')
+# # int must use comma, string can use either
+# print(name+' is', age)
+# print(name+' is from Idaho')
+
+print('Hi. \nHow are you?')
